53fc8724dc4846a864ed43559096189bb3415df5
Victor Lacasse-Beaudoin <victor.lacassebeaudoin@gmail.com>
2021-10-01 23:11:08 -0400

    Retirer flag example de cobra 'toggle'

--- cmd/root.go
+++ cmd/root.go
@@ -56,15 +56,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.haul.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
